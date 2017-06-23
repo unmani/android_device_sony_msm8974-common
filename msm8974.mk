3f570e29fe92e55c97df744f99a78b78fab6cccd
tomascus <arbiter1000@gmail.com>
2017-06-23 14:02:47 +1000

    msm8974-common: build gps hal
    Change-Id: Ia3e44dc325aeb4478a7804837b5d9144349bca1c

--- msm8974.mk
+++ msm8974.mk
@@ -111,6 +111,10 @@ PRODUCT_PACKAGES += \
     libmm-omxcore \
     libstagefrighthw
 
+# GPS
+PRODUCT_PACKAGES += \
+    gps.msm8974
+
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(COMMON_PATH)/overlay
 ifneq ($(BOARD_HAVE_RADIO),false)
